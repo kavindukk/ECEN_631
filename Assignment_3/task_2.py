b56155334a28a20f31d897f379bc568ae9d99c6d
kavindu <kavindukusal@gmail.com>
2021-03-01 17:57:08 -0700

    Assignment_3 task_3 completed

--- Assignment_3/task_2.py
+++ Assignment_3/task_2.py
@@ -2,12 +2,7 @@ import numpy as np
 import cv2
 import os
 
-def calculate_extrinsic_parameters(imageSet, CameraL, CameraR, chessBoardSize, paramL, paramR):
-    L_intrinsic = np.load('Parameters/'+ str(imageSet) + '/' + str(paramL)+'/intrinsic_parameters.npy') 
-    L_distortion = np.load('Parameters/'+ str(imageSet) + '/' + str(paramL)+'/distortion_parameters.npy')
-    R_intrinsic = np.load('Parameters/'+ str(imageSet) + '/' + str(paramR)+'/intrinsic_parameters.npy')
-    R_distortion = np.load('Parameters/'+ str(imageSet) + '/' + str(paramR)+'/distortion_parameters.npy')
-    
+def find_corners(imageSet, CameraL, CameraR, chessBoardSize):    
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
     objp = np.zeros((7*10,3), np.float32)				
     objp[:,:2] = chessBoardSize * np.mgrid[0:10,0:7].T.reshape(-1,2)	
@@ -32,16 +27,23 @@ def calculate_extrinsic_parameters(imageSet, CameraL, CameraR, chessBoardSize, p
                 elif head == str(CameraR):
                     R_imgpoints.append(corners2)
 
-        
                 img = cv2.drawChessboardCorners(img, (10,7), corners2,ret)
                 cv2.imshow('image',img)
                 cv2.waitKey(50)
-  
+    return objpoints, L_imgpoints, R_imgpoints, gray.shape[::-1]
+
+def  calculate_extrinsic_parameters(imageSet, CameraL, CameraR, chessBoardSize, paramL, paramR):
+    objpoints, L_imgpoints, R_imgpoints, shape = find_corners(imageSet, CameraL, CameraR, chessBoardSize)
+    L_intrinsic = np.load('Parameters/'+ str(imageSet) + '/' + str(paramL)+'/intrinsic_parameters.npy') 
+    L_distortion = np.load('Parameters/'+ str(imageSet) + '/' + str(paramL)+'/distortion_parameters.npy')
+    R_intrinsic = np.load('Parameters/'+ str(imageSet) + '/' + str(paramR)+'/intrinsic_parameters.npy')
+    R_distortion = np.load('Parameters/'+ str(imageSet) + '/' + str(paramR)+'/distortion_parameters.npy')
+
     termination_criteria_extrinsics = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 90, 1e-6)
     # termination_criteria_extrinsics = (cv2.TERM_CRITERIA_COUNT + cv2.TERM_CRITERIA_EPS, 90, 1e-6)
-    
+
     rms_stereo, stereo_camera_matrix_l, stereo_dist_coeffs_l, stereo_camera_matrix_r, stereo_dist_coeffs_r, R, T, E, F = \
-        cv2.stereoCalibrate(objpoints, L_imgpoints, R_imgpoints, L_intrinsic, L_distortion, R_intrinsic, R_distortion,  gray.shape[::-1], criteria=termination_criteria_extrinsics, flags=cv2.CALIB_FIX_INTRINSIC)
+        cv2.stereoCalibrate(objpoints, L_imgpoints, R_imgpoints, L_intrinsic, L_distortion, R_intrinsic, R_distortion,  shape, criteria=termination_criteria_extrinsics, flags=cv2.CALIB_FIX_INTRINSIC)
 
     print('R = ', R)
     print('T = ', T)
@@ -55,5 +57,5 @@ def calculate_extrinsic_parameters(imageSet, CameraL, CameraR, chessBoardSize, p
         np.save('fundamental_matrix.npy', F)
 
 
-calculate_extrinsic_parameters("Test_Images", "Stereo_Left", "Stereo_Right", 3.88, "Left_Cali", "Right_Cali")
-# calculate_extrinsic_parameters("Practice_Images", "SL", "SR", 2.,"L", "R")
+# calculate_extrinsic_parameters("Test_Images", "Stereo_Left", "Stereo_Right", 3.88, "Left_Cali", "Right_Cali")
+calculate_extrinsic_parameters("Practice_Images", "SL", "SR", 2.,"L", "R")
