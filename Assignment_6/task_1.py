cc4cb735b0c485f4e4e55c4c36723a5456761425
kavindu <kavindukusal@gmail.com>
2021-03-28 22:43:07 -0600

    task1 completed

--- Assignment_6/task_1.py
+++ Assignment_6/task_1.py
@@ -1,38 +1,22 @@
 import numpy as np
-import cv2
+import cv2 
 
+cimg1 = cv2.imread('Downloads/im1.jpg')          
+cimg2 = cv2.imread('Downloads/b1.png') 
+img1 = cv2.imread('Downloads/im1.jpg',cv2.IMREAD_GRAYSCALE)          
+img2 = cv2.imread('Downloads/b1.png',cv2.IMREAD_GRAYSCALE) 
 
-picture = cv2.imread('Downloads/im1.jpg', 0)
-image = cv2.imread('Downloads/b1.png', 0) # (756,495)
-print('image:', image.shape)
-
-# method of feature points
-# sift = cv2.xfeatures2d.SIFT_create()
-# surf = cv2.xfeatures2d.SURF_create()
 orb = cv2.ORB_create()
 
-# using ORB as my match method
-pic_kp, pic_des = orb.detectAndCompute(picture, None)
-img_kp, img_des = orb.detectAndCompute(image, None)
-# pic_kp = cv2.drawKeypoints(picture, pic_kp, None)
-# img_kp = cv2.drawKeypoints(image, img_kp, None)
+kp1, des1 = orb.detectAndCompute(img1,None)
+kp2, des2 = orb.detectAndCompute(img2,None)
 
-# Brute Force Matching
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-matches = bf.match(pic_des, img_des)
-# sort the distance from lower to higher
-matches = sorted(matches, key=lambda x:x.distance)
+matches = bf.match(des1,des2)
+matches = sorted(matches, key = lambda x:x.distance)
+img3 = cv2.drawMatches(cimg1,kp1,cimg2,kp2,matches[:50],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
-# show how many match in picture and image
-# the smaller distance the better is matched
-print(len(matches))
-for m in matches:
-    print(m.distance)
-
-matching_result = cv2.drawMatches(image,img_kp,picture,pic_kp,matches[:10],None,flags=2)
-
-
-cv2.imshow('matching_result',matching_result)
-cv2.imwrite('Task1_Matching_Result.jpg',matching_result)
+cv2.imshow('Task1_Matching_Results', img3)
+cv2.imwrite('Task1_Matching_Result.jpg',img3)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
